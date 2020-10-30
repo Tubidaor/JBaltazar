a2bf184b3ff49e9e5d0cf97a368ffd382ea0a5de
Juan Baltazar <juan.baltazar1@gmail.com>
2020-10-29 20:05:33 -0700

    changed background color

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -9,60 +9,62 @@ export default function Projects(props) {
     <div key={proj.id} className="projectsDiv">
       <section className="project number-1">
         <h3 className="projectsH3">{proj.title}</h3>
-        <h5 className="projectH5">
-          Date Created:
-            <span className="dateCreatedSpan"> {proj.date_completed}</span>
-        </h5>
-        <p className="projectsP">
-          {proj.description}
-          <br/><br/>
-          Technologies used:
-          <br/><br/>
-          {
-            proj.Tech.map(techL =>
-              <img
+        <div className="projectCon">
+          <h5 className="projectH5">
+            Date Created:
+              <span className="dateCreatedSpan"> {proj.date_completed}</span>
+          </h5>
+          <p className="projectsP">
+            {proj.description}
+            <br/><br/>
+            Technologies used:
+            <br/><br/>
+            {
+              proj.Tech.map(techL =>
+                <img
                 className="techLogos"
                 alt={techL.name+"logo"}
                 src={techL.url}
-              />
-            )
-          }
-          </p>
-        <img 
-          src={proj.imageSource}
-          url={proj.imageSource}
-          alt={proj.imageAlt}
-        />
-        <div className="buttonContainer">
-            <button
-              type="button"
-              className={`button ${proj.id}`}
-              onClick={(e) =>
-                {e.preventDefault(); window.open(proj.projectRepoClient)}
+                />
+                )
               }
-            >
-              Client Repo
-            </button>
-            { proj.projectRepoServer !== 'None' &&
+            </p>
+          <img 
+            src={proj.imageSource}
+            url={proj.imageSource}
+            alt={proj.imageAlt}
+            />
+          <div className="buttonContainer">
               <button
+                type="button"
+                className={`button ${proj.id}`}
+                onClick={(e) =>
+                  {e.preventDefault(); window.open(proj.projectRepoClient)}
+                }
+                >
+                Client Repo
+              </button>
+              { proj.projectRepoServer !== 'None' &&
+                <button
                 type="button"
                 className={`button ${proj.id}`}
                 onClick={(e) =>
                   {e.preventDefault(); window.open(proj.projectRepoServer)}
                 }
-              >
-                Server Repo
-              </button>
-            }
-            <button
-              type="button"
-              className={`button ${proj.id}`}
-              onClick={(e) =>
-                  {e.preventDefault(); window.open(proj.projectURL)}
+                >
+                  Server Repo
+                </button>
               }
-              >
-              Web App
-            </button>
+              <button
+                type="button"
+                className={`button ${proj.id}`}
+                onClick={(e) =>
+                  {e.preventDefault(); window.open(proj.projectURL)}
+                }
+                >
+                Web App
+              </button>
+          </div>
         </div>
       </section>
   </div>
